f0de450ef4896f3e22e4ffaeaa1be2f62f8bbd39
devrahoriya <devrahoriya031@gmail.com>
2021-06-25 17:47:46 +0530

    2nd commmand

--- src/main/java/com/learn/course/hello/HelloController.java
+++ src/main/java/com/learn/course/hello/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	@RequestMapping("/hello")
+	@RequestMapping("/app")
 	public String sayHi() {
-	return "Hi";
+	return "Application";
 	}
 }
